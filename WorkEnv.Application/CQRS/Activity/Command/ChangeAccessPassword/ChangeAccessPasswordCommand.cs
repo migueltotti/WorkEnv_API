3dc02bed87cb99c1bd254be2c3a8778ea6e73672
Miguel Totti <migueltotti2005@gmail.com>
2025-02-22 11:43:18 -0300

    refactor(command): add access password return

--- WorkEnv.Application/CQRS/Activity/Command/ChangeAccessPassword/ChangeAccessPasswordCommand.cs
+++ WorkEnv.Application/CQRS/Activity/Command/ChangeAccessPassword/ChangeAccessPasswordCommand.cs
@@ -1,6 +1,7 @@
 using MediatR;
+using WorkEnv.Application.Result;
 
 namespace WorkEnv.Application.CQRS.Activity.Command.ChangeAccessPassword;
 
-public record ChangeAccessPasswordCommand( Guid activityId, Guid adminOrOwnerId)
-    : IRequest<Result.Result>;
+public record ChangeAccessPasswordCommand(Guid activityId, Guid adminOrOwnerId)
+    : IRequest<Result<object>>;
--- WorkEnv.Application/CQRS/Activity/Command/ChangeAccessPassword/ChangeAccessPasswordCommandHandler.cs
+++ WorkEnv.Application/CQRS/Activity/Command/ChangeAccessPassword/ChangeAccessPasswordCommandHandler.cs
@@ -4,7 +4,7 @@ using WorkEnv.Domain.Interfaces;
 
 namespace WorkEnv.Application.CQRS.Activity.Command.ChangeAccessPassword;
 
-public class ChangeAccessPasswordCommandHandler : IRequestHandler<ChangeAccessPasswordCommand, Result.Result>
+public class ChangeAccessPasswordCommandHandler : IRequestHandler<ChangeAccessPasswordCommand, Result<object>>
 {
     private readonly IUnitOfWork _uof;
 
@@ -13,27 +13,27 @@ public class ChangeAccessPasswordCommandHandler : IRequestHandler<ChangeAccessPa
         _uof = uof;
     }   
     
-    public async Task<Result.Result> Handle(ChangeAccessPasswordCommand request, CancellationToken cancellationToken)
+    public async Task<Result<object>> Handle(ChangeAccessPasswordCommand request, CancellationToken cancellationToken)
     {
         var activity = await _uof.ActivityRepository.GetByIdAsync(request.activityId, cancellationToken);
 
         if (activity is null)
-            return Result.Result.Failure(ActivityErrors.ActivityNotFound);
+            return Result<object>.Failure(ActivityErrors.ActivityNotFound);
 
         if (!activity.AdminId.Equals(request.adminOrOwnerId) && 
             !activity.WorkSpace.OwnerId.Equals(request.adminOrOwnerId))
-            return Result.Result.Failure(ActivityErrors.AdminOrOwnerIdInvalid);
+            return Result<object>.Failure(ActivityErrors.AdminOrOwnerIdInvalid);
 
         var adminOrOwner = await _uof.UserRepository.GetByIdAsync(request.adminOrOwnerId, cancellationToken);
         
         if(adminOrOwner is null)
-            return Result.Result.Failure(UserErrors.UserNotFound);
+            return Result<object>.Failure(UserErrors.UserNotFound);
         
         activity.ChangeAccessPassword(adminOrOwner.UserId);
         
         _uof.ActivityRepository.Update(activity);
         await _uof.CommitChangesAsync(cancellationToken);
         
-        return Result.Result.Success();
+        return Result<object>.Success(activity.AccessPassword);
     }
 }
